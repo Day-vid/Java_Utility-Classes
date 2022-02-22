b4c5411e0bbe57025d9344cd556576971a3c00d1
Day-vid <44570531+Day-vid@users.noreply.github.com>
2022-02-22 07:41:51 +0000

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+/**
+* Input Class which enables users to input data into the program.
+*
+* @author Ravish Goyal, Mark Creado used by David Bryant
+* @version 02 February 2022
+*/
+public class Input
+{
+    /**
+    * Default Constructor for objects of Input class
+    */
+    public Input()
+    {
+
+    }
+
+    /**
+    * Method to get character input from keyboard
+    *
+    * @param    displayMessage  A message in String to inform user of the input
+    * @param    position    The index position for the desired character  
+    * @return   The character from user input based on the index position
+    */
+    public char acceptCharInput(String displayMessage, int position)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.println(displayMessage);
+        String input = console.nextLine();
+        return input.charAt(position);
+    }
+
+    /**
+    * Method to get number input from keyboard
+    *
+    * @param    displayMessage  A message in String to inform user of the input
+    * @return   The number from user input based on the number
+    */
+    public double acceptDoubleInput(String displayMessage)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.println(displayMessage);
+        return Double.parseDouble(console.nextLine());
+    }
+
+    /**
+    * Method to get number input from keyboard
+    *
+    * @param    displayMessage  A message in String to inform user of the input
+    * @return   The number from user input based on the index position
+    */
+    public int acceptIntegerInput(String displayMessage)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.println(displayMessage);
+        int input = Integer.parseInt(console.nextLine());
+        return input;
+    }
+
+    /**
+    * Method to get string input from keyboard
+    *
+    * @param    displayMessage  A message in String to inform user of the input
+    * @return   The string from user input based on the index position
+    */
+    public String acceptStringInput(String displayMessage)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.println(displayMessage);
+        String input = console.nextLine();
+        return input;
+    }
+
+    /**
+    * Method to get string input from keyboard
+    *
+    * @param    displaymessage  A message in String to inform user of the input
+    * @return   The string from user input based on the index position
+    */
+    public String acceptInput(String displayMessage)
+    {
+        Scanner console = new Scanner(System.in);
+        System.out.println(displayMessage);
+        String input = console.nextLine();
+        return input;
+    }
+}
---
+++ Validation.java
@@ -0,0 +1,46 @@
+/**
+* Validation Class which enables user input validation for the program.
+*
+* @author Ravish Gould and used by David Bryant
+* @version 04 February 2022
+*/
+public class Validation
+{
+    /**
+    * Constructor for objects of Validation class
+    */
+    public Validation()
+    {
+
+    }
+
+    /**
+    * Class method to get check against blank input from user
+    *
+    * @param    value   The value that needs to be validated
+    * @return   blank   boolean value to ascertain if the String is blank
+    */
+    public boolean stringIsBlank(String value)
+    {
+        boolean blank = false;
+        if (value.trim().length() == 0)
+            blank = true;
+        return blank;
+    }
+
+    /**
+    * Validation Class Method to check if the input value from the user is within range
+    *
+    * @param    value   The value that needs to be validated
+    * @param    min     the minimum value set for validation
+    * @param    max     the maximum value set for validation
+    * @return   withinRange     returns the value whether the input was true or false based on validation logic
+    */
+    public boolean stringIsWithinRange(String value, int min, int max)
+    {
+        boolean withinRange = false;
+        if ((value.trim().length() >= min) && (value.trim().length() <= max ))
+            withinRange = true;
+        return withinRange;
+    }
+}
